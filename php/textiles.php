25a3803c75a5f29b13a07f2b4c7aca9edacd2e26
Kanana2024 <kanananatasha8@gmail.com>
2026-07-25 11:42:54 +0300

    Added textiles product page

--- php/textiles.php
+++ php/textiles.php
@@ -2,41 +2,43 @@
 session_start();
 include("connection.php");
 
-$sql = "SELECT * FROM products WHERE category='Cleaning'";
+$sql = "SELECT * FROM products WHERE category='Textiles'";
 $result = mysqli_query($conn,$sql);
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Products - Cleaning</title>
+    <title>Products - Textiles</title>
     <link rel ="stylesheet" href="../css/products.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
 </head>
-<body>
-    <nav>
-        <h3>Move-In Budget</h3>
 
-        <ol>
-            <li><a href="../html/home.html"><i class="fas fa-home"></i> Home</a></li>
-            <li><a href="livingroom.php"><i class="fas fa-couch"></i> Products</a></li>
-            <li><a href="cartpage.php"><i class="fa fa-shopping-cart"></i> Cart</a></li>
-        </ol>
-    </nav>
-    <div class="section-title"><h2>Cleaning products</h2></div>
-        <section class="products_page">
-            <aside class="categories">
-                <h2>Categories</h2>
-                <ul>
-                    <li><i class="fas fa-chevron-right"></i> <a href="furniture.php">Furniture</a></li>
-                    <li><i class="fas fa-chevron-right"></i> <a href="electronics.php">Electronics</a></li>
-                    <li><i class="fas fa-chevron-right"></i> <a href="textiles.php">Textiles</a></li>
-                    <li><i class="fas fa-chevron-right"></i> <a href="utensils.php">Utensils</a></li>
-                    <li><i class="fas fa-chevron-right"></i> <a href="cleaning.php">Cleaning</a></li>
-                </ul>
-            </aside>
-           <div class = "products_container">
+<body>
+<nav>
+    <h3>Move-In Budget</h3>
+     <ol>
+        <li><a href="../html/home.html"><i class="fas fa-home"></i> Home</a></li>
+        <li><a href="products.html"><i class="fas fa-couch"></i> Products</a></li>
+        <li><a href="cartpage.php"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+    </ol>
+</nav>
+
+<div class="section-title"><h2>Textiles</h2></div>
+
+<section class="products_page">
+    <aside class="categories">
+        <h2>Categories</h2>
+        <ul>
+            <li><i class="fas fa-chevron-right"></i> <a href="furniture.php">Furniture</a></li>
+            <li><i class="fas fa-chevron-right"></i> <a href="electronics.php">Electronics</a></li>
+            <li><i class="fas fa-chevron-right"></i> <a href="textiles.php">Textiles</a></li>
+            <li><i class="fas fa-chevron-right"></i> <a href="utensils.php">Utensils</a></li>
+            <li><i class="fas fa-chevron-right"></i> <a href="cleaning.php">Cleaning</a></li>
+        </ul>
+    </aside>
+    <div class = "products_container">
         <?php while($row = mysqli_fetch_assoc($result)): ?>
             <form method="post" action="cart.php">
                 <div class="furniture_card">
@@ -55,9 +57,9 @@ $result = mysqli_query($conn,$sql);
             </form>
         <?php endwhile; ?>
         </div>
-        </section>
+</section>
 <p style="text-align:center;background-color: #90e0ef; padding: 23px;margin:auto">
-    &copy 2026 Moving out budget list. All rights reserved.
+&copy 2026 Moving out budget list. All rights reserved.
 </p>
 
 </body>
